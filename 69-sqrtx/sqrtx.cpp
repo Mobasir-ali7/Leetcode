63cae8814cfb37e26bf4665400ad48c765bbdd67
Mobasir Ali <mobasirali1357@gmail.com>
2026-07-29 23:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.77%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int start = 1;
+        int end = x , mid;
+        int ans = 0;
+
+        while(start<=end){
+            mid = start + (end-start)/2;
+
+            if(mid == x/mid && x % mid ==0){
+                return mid;
+
+            }else if(mid <= x/mid){
+                ans = mid;
+                start = mid + 1;
+            }else{
+                end = mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
